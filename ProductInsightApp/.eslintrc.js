3baffd523524129dcf7a6aa827de739dd2fca415
Rohan Acharya <rohanacharya42@gmail.com>
2024-09-20 12:58:27 +0545

    fix: correct ESLint setup and clean root package files

--- ProductInsightApp/.eslintrc.js
+++ ProductInsightApp/.eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/no-unstable-nested-components': 'off', // Turn off the nested component warning
+    '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}], // Adjust unused vars rule
+    '@typescript-eslint/no-require-imports': 'off', // Disable the require rule for this file becuase if not app won't load
+  },
 };
